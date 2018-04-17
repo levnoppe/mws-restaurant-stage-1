19ab8eb79ffe7b46b31633d138be875f1b7a2cef
Lev <33432198+levnoppe@users.noreply.github.com>
2018-04-17 21:31:32 +0300

    commit3

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -88,7 +88,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   image.srcset= setString
   image.sizes=sizeString
 
-
+  image.alt =  `Image of ${restaurant.name} restaurant` 
+  
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
 
@@ -125,7 +126,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
   title.innerHTML = 'Reviews';
   title.tabIndex="0";
   container.appendChild(title);
@@ -178,6 +179,7 @@ fillBreadcrumb = (restaurant=self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
+  li.setAttribute('aria-current', 'page');
   breadcrumb.appendChild(li);
 }
 
